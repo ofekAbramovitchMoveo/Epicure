e575519fde1be2263d3a57fe0892b9e571ae6090
ofekAbramovitchMoveo <ofeka@moveo.co.il>
2024-09-25 11:25:15 +0300

    changed scrolltotop to not scroll to top when inside the same restaurant details page

--- src/components/scroll-to-top.tsx
+++ src/components/scroll-to-top.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import { useLocation } from "react-router"
 
 interface ScrollToTopProps {
@@ -7,9 +7,21 @@ interface ScrollToTopProps {
 
 export default function ScrollToTop({ children }: ScrollToTopProps) {
     const location = useLocation()
+    const prevPathnameRef = useRef<string | null>(null)
+
 
     useEffect(() => {
-        window.scrollTo(0, 0)
+        const prevPathname = prevPathnameRef.current
+        prevPathnameRef.current = location.pathname
+
+        const isSameRestaurantDetails = prevPathname?.startsWith('/restaurant/') && location.pathname.startsWith('/restaurant/')
+        const isDifferentMealTime = (prevPathname?.includes('/lunch') || prevPathname?.includes('/dinner') || prevPathname === `/restaurant/${location.pathname.split('/')[2]}`) &&
+            (location.pathname.includes('/lunch') || location.pathname.includes('/dinner') || location.pathname === `/restaurant/${location.pathname.split('/')[2]}`)
+
+
+        if (!isSameRestaurantDetails && !isDifferentMealTime) {
+            window.scrollTo(0, 0)
+        }
     }, [location.pathname])
 
     return <>{children}</>
